cff6e7836a8c8291d33505df77767ac8c5fa2dd6
Eugene Ware <eugene@noblesamurai.com>
2014-10-22 16:26:58 +1100

    have done level = 1

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-var request = require('request');
+var request = require('request').defaults({ jar: true }),
+    after = require('after');
+
 module.exports = suggest;
 
-var urlBase = 'http://clients1.google.com/complete/search?client=heirloom-hp&hl=en&gs_rn=0&gs_ri=heirloom-hp&cp=7&gs_id=0&q='
+var urlBase = 'http://clients1.google.com/complete/search?client=heirloom-hp&hl=en&gs_rn=0&gs_ri=heirloom-hp&cp=7&'
 var resRegex = /^window\.google\.ac\.h\((.*)\)$/;
 var userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36';
+var reqId = 0;
 
-function suggest(keyword, cb) {
+function suggest(keyword, opts, cb) {
+  if (typeof cb === 'undefined' && typeof opts === 'function') {
+    cb = opts;
+    opts = {};
+  }
+
+  opts.levels = opts.levels || 0;
+  if (opts.levels === 0) {
+    doSuggest(keyword, cb);
+  } else if (opts.levels === 1) {
+    var nA = 'a'.charCodeAt(0);
+    var next = after(26, done);
+    var results = [];
+
+    for (var i = 0; i < 26; i++) {
+      c = String.fromCharCode(nA + i);
+      doSuggest(keyword + ' ' + c, function (err, suggestions) {
+        if (err) return next(err);
+        results = results.concat(suggestions);
+        next();
+      });
+    }
+    function done () {
+      cb(null, results);
+    }
+  } else {
+    doSuggest(keyword, cb);
+  }
+}
+
+function doSuggest(keyword, cb) {
   request({
-    url: urlBase + encodeURIComponent(keyword),
+    url: urlBase + 'gs_id=' + (reqId++) + '&q=' + encodeURIComponent(keyword),
     headers: { 'User-Agent': userAgent }
   }, function (err, res, body) {
     if (err) return cb(err);
--- test/index.js
+++ test/index.js
@@ -5,9 +5,20 @@ it('should be able to suggest a keyword', function(t) {
   suggest('dog training', function (err, suggestions) {
     t.plan(2);
     if (err) throw err;
-    t.assert(suggestions.length > 0);
+    t.equals(suggestions.length, 10);
     t.assert(suggestions.some(function (suggestion) {
       return ~suggestion.indexOf('dog training');
     }));
   })
+});
+
+it('should be able to traverse a level', function(t) {
+  suggest('dog training', { levels: 1 }, function (err, suggestions) {
+    if (err) throw err;
+    t.assert(suggestions.length, 260);
+    t.assert(suggestions.some(function (suggestion) {
+      return ~suggestion.indexOf('dog training');
+    }));
+    t.end();
+  });
 });
